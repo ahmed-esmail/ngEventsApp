6ce629196bf9c00ffd569d16173d4a243479a7ec
ahmed.esmail <ahmed.esmail.gm@gmail.com>
2022-06-29 03:35:30 +0200

    profile

--- src/app/user/profile/profile.component.ts
+++ src/app/user/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup} from "@angular/forms";
+import {AuthService} from "../auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  profileForm!: FormGroup;
+  constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit(): void {
+    let firstName = new FormControl(this.authService.currentUser.firstName);
+    let lastName = new FormControl(this.authService.currentUser.lastName);
+    this.profileForm = new FormGroup({firstName: firstName, lastName: lastName});
   }
 
+  handleSave(value: any) {
+    this.authService.updateCurrentUser(value.firstName, value.lastName);
+  }
+
+  handleCancel() {
+    this.router.navigate(['events']);
+  }
 }
