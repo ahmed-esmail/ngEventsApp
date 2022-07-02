3c6e9689ec8553096f9cfac240c02d1c2daae957
ahmed.esmail <ahmed.esmail.gm@gmail.com>
2022-07-03 00:00:31 +0200

    modal

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,13 +1,13 @@
-import {Component, OnInit} from '@angular/core';
-import {EventService} from "../../shared";
-import {ActivatedRoute} from "@angular/router";
-import {IEvent} from "../../models/IEvent";
-import {ISession} from "../../models/ISession";
+import { Component, OnInit } from '@angular/core';
+import { EventService } from '../../shared';
+import { ActivatedRoute } from '@angular/router';
+import { IEvent } from '../../models/IEvent';
+import { ISession } from '../../models/ISession';
 
 @Component({
   selector: 'app-event-details',
   templateUrl: './event-details.component.html',
-  styleUrls: ['./event-details.component.scss']
+  styleUrls: ['./event-details.component.scss'],
 })
 export class EventDetailsComponent implements OnInit {
   event!: IEvent;
@@ -15,20 +15,27 @@ export class EventDetailsComponent implements OnInit {
   filterBy: string = 'all';
   sortBy: string = 'votes';
 
-  constructor(private eventService: EventService, private route: ActivatedRoute) {
-  }
+  constructor(
+    private eventService: EventService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
-    let id: number = +this.route.snapshot.params['id'];
-    this.event = this.eventService.getEvent(id);
+    this.route.params.subscribe((params) => {
+      let id = +params['id'];
+      this.event = this.eventService.getEvent(id);
+      this.addMode = false;
+    });
   }
 
   addSessionToEvent(session: ISession) {
-    // @ts-ignore
-    const nextId = Math.max.apply(null, this.event.sessions.map(s => s.id));
-    session.id = nextId + 1
-    this.event.sessions.push(session)
-    this.eventService.updateEvent(this.event)
-    this.addMode = false
+    const nextId = Math.max.apply(
+      null,
+      this.event.sessions.map((s) => s.id as number)
+    );
+    session.id = nextId + 1;
+    this.event.sessions.push(session);
+    this.eventService.updateEvent(this.event);
+    this.addMode = false;
   }
 }
--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from "../user/auth.service";
 import {IUser} from "../models/IUser";
+import {ISession} from "../models/ISession";
+import {EventService} from "../shared";
 
 @Component({
   selector: 'app-nav',
@@ -8,12 +10,21 @@ import {IUser} from "../models/IUser";
   styleUrls: ['./nav.component.scss']
 })
 export class NavComponent implements OnInit {
+  searchTerm: string = '';
+  foundSessions!: ISession[];
 
-  constructor(public authService: AuthService) {
+  constructor(public authService: AuthService, private eventService: EventService) {
   }
-
 
   ngOnInit(): void {
   }
 
+  searchSessions(searchTerm: string) {
+    this.eventService.searchSessions(searchTerm).subscribe({
+      next: sessions => {
+        this.foundSessions = sessions
+        console.log(this.foundSessions)
+      }
+    });
+  }
 }
