e3cd55889b7d7823147efa3d289f961db2440830
ahmed.esmail <ahmed.esmail.gm@gmail.com>
2022-07-09 12:36:22 +0200

    login service

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -1,19 +1,33 @@
 import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {catchError, of, tap} from "rxjs";
+import {IUser} from "../models/IUser";
+import {TokenStorageService} from "../common/token-storage.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  currentUser: any;
+  currentUser!: IUser;
 
+  constructor(private http: HttpClient, private tokenStorage: TokenStorageService) {
+    if (this.tokenStorage.getUser().userName) {
+      this.currentUser = this.tokenStorage.getUser();
+    }
+  }
 
-  loginUser(userName: string, password: string) {
-    this.currentUser = {
-      id: 1,
-      userName: userName,
-      firstName: 'John',
-      lastName: 'papa'
-    };
+  loginUser(username: string, password: string) {
+
+    let options = {headers: new HttpHeaders({'Content-type': 'application/json'})}
+    return this.http.post<any>('/api/login', {username, password}, options).pipe(
+      tap(data => {
+        this.currentUser = <IUser>data['user'];
+        this.tokenStorage.saveUser(this.currentUser)
+      })).pipe(
+        catchError(err => {
+          return of(false);
+        })
+    )
 
   }
 
--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -11,14 +11,23 @@ export class LoginComponent {
   username: string = '';
   password: string = '';
   mouseoverLogin: any
+  loginInvalid: boolean = false;
 
   constructor(private authService: AuthService, private router: Router) {
 
   }
 
   login(formValues) {
-    this.authService.loginUser(formValues.userName, formValues.password)
-    this.router.navigate(['events'])
+    this.authService.loginUser(formValues.userName, formValues.password).subscribe(
+        (response) => {
+          console.log(response);
+          if (!response) {
+            this.loginInvalid = true;
+          } else {
+            this.router.navigate(['/events'])
+          }
+        }
+    )
   }
 
   cancel() {
